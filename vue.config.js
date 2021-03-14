76c7fbe9489f779f13082c5f0988bd28186a1d8f
Anastasia <nancy56@list.ru>
2021-03-14 10:37:44 +0800

    upd vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  publicPath: '/vuetify-to-do-list/'
+}
+
+module.exports = {
   transpileDependencies: [
     'vuetify'
   ]
-}
-module.exports = {
-  publicPath: '/vuetify-to-do-list/'
 }
